e5a25cd3981476f287095ab3c6ad794b6d6c3686
Yasin Aksu <yasinaksu88@gmail.com>
2021-02-25 21:39:11 +0300

    entitiy classes created in core layer

---
+++ JwtDemo/JwtDemo.AuthServer.Core/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JwtDemo.AuthServer.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
---
+++ JwtDemo/JwtDemo.AuthServer.Core/Models/UserApp.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JwtDemo.AuthServer.Core.Models
+{
+    public class UserApp : IdentityUser
+    {
+        public string City { get; set; }
+    }
+}
