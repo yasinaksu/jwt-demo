5d814cac45c0ea2161e548a2997ea4d2d4fb84f6
Yasin Aksu <yasinaksu88@gmail.com>
2021-02-25 22:14:04 +0300

    error dto

---
+++ JwtDemo/JwtDemo.Shared/Dtos/ErrorDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JwtDemo.Shared.Dtos
+{
+    public class ErrorDto
+    {
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public ErrorDto(string error, bool isShow) : this()
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+        public ErrorDto(List<string> errors, bool isShow) : this()
+        {
+            Errors.AddRange(errors);
+            IsShow = isShow;
+        }
+        public List<string> Errors { get; private set; }
+        public bool IsShow { get; private set; }
+    }
+}
