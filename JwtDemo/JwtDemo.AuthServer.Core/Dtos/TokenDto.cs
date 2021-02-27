220caae6887c198a2c063e4f58dda9b70679fa5a
Yasin Aksu <yasinaksu88@gmail.com>
2021-02-27 21:45:48 +0300

    creating TokenDto and ClientTokenDto

---
+++ JwtDemo/JwtDemo.AuthServer.Core/Dtos/TokenDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JwtDemo.AuthServer.Core.Dtos
+{
+    public class TokenDto
+    {
+        public string AccessToken { get; set; }
+        public DateTime AccessTokenExpiration { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
+
+    }
+}
