a22b508745d5b2e7c5a755c5fe5be3933284a723
Yasin Aksu <yasinaksu88@gmail.com>
2021-02-28 16:13:14 +0300

    creating IUnitOfWork interface

---
+++ JwtDemo/JwtDemo.AuthServer.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtDemo.AuthServer.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
