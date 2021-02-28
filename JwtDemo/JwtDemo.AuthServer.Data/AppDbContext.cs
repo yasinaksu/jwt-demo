5fe06d7c0c163c24310ddea6fd7daf70fd6f41a8
Yasin Aksu <yasinaksu88@gmail.com>
2021-02-28 22:27:45 +0300

    AppDbContext class were created

---
+++ JwtDemo/JwtDemo.AuthServer.Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using JwtDemo.AuthServer.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JwtDemo.AuthServer.Data
+{
+    public class AppDbContext : IdentityDbContext<UserApp, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+    }
+}
