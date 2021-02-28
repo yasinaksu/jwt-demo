babf7e4736b7efc71e9a8f4be9dabaaa9c93f5e1
Yasin Aksu <yasinaksu88@gmail.com>
2021-02-28 15:26:39 +0300

    creatin IUserService interface

---
+++ JwtDemo/JwtDemo.AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using JwtDemo.AuthServer.Core.Dtos;
+using JwtDemo.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtDemo.AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAppAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByUserNameAsync(string userName);
+    }
+}
