f1a2b8536bce0f3bbd1c047e8f64127dff842445
Yasin Aksu <yasinaksu@tekuzem.com>
2021-03-05 10:24:10 +0300

    options patters configurations

--- JwtDemo/JwtDemo.AuthServer.WebAPI/Startup.cs
+++ JwtDemo/JwtDemo.AuthServer.WebAPI/Startup.cs
@@ -1,3 +1,4 @@
+using JwtDemo.Shared.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,7 @@ namespace JwtDemo.AuthServer.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.Configure<CustomTokenOption>(Configuration.GetSection("TokenOptions"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
---
+++ JwtDemo/JwtDemo.Shared/Configurations/CustomTokenOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JwtDemo.Shared.Configurations
+{
+    public class CustomTokenOption
+    {
+        public List<string> Audience { get; set; }
+        public string Issuer { get; set; }
+        public int AccessTokenExpiration { get; set; }
+        public int RefreshTokenExpiration { get; set; }
+        public string SecurityKey { get; set; }
+    }
+}
