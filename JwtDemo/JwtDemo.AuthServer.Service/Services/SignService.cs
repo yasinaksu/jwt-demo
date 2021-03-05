bd8db7c047ea7462e3bd08cf1d88b0d37cc9da44
Yasin Aksu <yasinaksu@tekuzem.com>
2021-03-05 10:08:32 +0300

    sign service implemantations (SymmetricSecurityKey generator)

---
+++ JwtDemo/JwtDemo.AuthServer.Service/Services/SignService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JwtDemo.AuthServer.Service.Services
+{
+    public static class SignService
+    {
+        public static SecurityKey GetSymmetricSecurity(string securityKey)
+        {
+            var encodedSecurityKey = Encoding.UTF8.GetBytes(securityKey);
+            var symmetricSecurityKey = new SymmetricSecurityKey(encodedSecurityKey);
+            return symmetricSecurityKey;
+        }
+    }
+}
