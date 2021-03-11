3fe627546c87d80d967b5bb73eb38ce620bc9b47
Yasin Aksu <yasinaksu@tekuzem.com>
2021-03-11 10:21:01 +0300

    id changed from int to string

--- JwtDemo/JwtDemo.AuthServer.Core/Dtos/UserAppDto.cs
+++ JwtDemo/JwtDemo.AuthServer.Core/Dtos/UserAppDto.cs
@@ -6,7 +6,7 @@ namespace JwtDemo.AuthServer.Core.Dtos
 {
     public class UserAppDto
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string City { get; set; }
